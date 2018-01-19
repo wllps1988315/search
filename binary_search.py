ef424048d128bc2f02d9c147f04779ae6ef5014f
wanglan <wang_lan@gowild.cn>
2018-01-19 11:03:02 +0800

    binary_search

--- binary_search.py
+++ binary_search.py
@@ -20,3 +20,27 @@ def binarySearch(alist,item):
             else:
                 first = midpoint+1
     return found
+
+def binarySearch(alist, item):
+    '''
+    二分查找recursive version
+    :param alist:
+    :param item:
+    :return:
+    '''
+    if len(alist) == 0:
+        return False
+    else:
+        midpoint = len(alist)//2
+        if alist[midpoint]==item:
+          return True
+        else:
+          if item<alist[midpoint]:
+            return binarySearch(alist[:midpoint],item)
+          else:
+            return binarySearch(alist[midpoint+1:],item)
+
+if __name__ == '__main__':
+    testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+    print(binarySearch(testlist, 3))
+    print(binarySearch(testlist, 13))
